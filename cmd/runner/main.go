c8cf980e60821c75384d54ead96d3141c6b9a76a
Jack Perry <jack.e.perry@outlook.com>
2022-01-23 12:18:34 -0700

    👌 IMPROVE: basic cmdline flag parsing added

--- cmd/runner/main.go
+++ cmd/runner/main.go
@@ -1,7 +1,29 @@
 package runner
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"strings"
+)
 
 func Run() {
-	fmt.Println("Hello World")
+	filenamePtr := flag.String("filename", "", "Name of the file being seeked")
+	seekLinesPtr := flag.Int("lines", 10, "Number of lines to seek, defaults to 10.")
+
+	flag.Parse()
+
+	filenameIsValid := validateFilename(*filenamePtr)
+	if !filenameIsValid {
+		log.Fatalln("Invalid filename (must be available CSV file):", *filenamePtr)
+	}
+
+	_ = seekLinesPtr
+}
+
+func validateFilename(filename string) bool {
+	if !strings.HasSuffix(filename, ".csv") || len(filename) < 4 {
+		return false
+	}
+
+	return true
 }
